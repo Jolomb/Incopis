74653400b76a73cb10163dccff4f555c7a5c6ed3
Jolomb <Jolomb@Mail.com>
2020-08-23 00:30:07 +0300

    add nginx deployment

--- Incopis_Front/Incopis_Front/settings.py
+++ Incopis_Front/Incopis_Front/settings.py
@@ -120,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = "/mnt/g/EyalBackup/Projects/Incubas/WebFrameWork/Incopis_Front/static_root/"
