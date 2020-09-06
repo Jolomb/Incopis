1e4f4229402f71be2925b81c0cede9401c3ab5aa
Jolomb <Jolomb@Mail.com>
2020-09-06 22:32:38 +0300

    cache reddis the brand details page

--- Incopis_Front/wardrobe_manager/urls.py
+++ Incopis_Front/wardrobe_manager/urls.py
@@ -1,10 +1,15 @@
+from django.conf import settings
+from django.views.decorators.cache import cache_page
+from django.core.cache.backends.base import DEFAULT_TIMEOUT
 from django.urls import path
 from . import views
+
+CACHE_TTL = getattr(settings, 'CACHE_TTL', DEFAULT_TIMEOUT)
 
 app_name = 'wardrobe_manager'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('brand/<int:pk>/', views.BrandDetail.as_view(), name='brand_details'),
+    path('brand/<int:pk>/', cache_page(CACHE_TTL)(views.BrandDetail.as_view()), name='brand_details'),
     path('brand/<int:brand_id>/rating', views.brand_rate, name='brand_rate'),
     path('brand/', views.AllBrandsView.as_view(), name='all_brands')
 ]
