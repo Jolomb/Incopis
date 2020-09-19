b5cb174d771e7136917e4b66516dd65fc5969e71
Jolomb <eyalrs21@gmail.com>
2020-09-19 19:12:25 +0300

    Support for user defined items

--- Incopis_Front/wardrobe_manager/admin.py
+++ Incopis_Front/wardrobe_manager/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Brand, Item
+from .models import Brand, Item, Shopper
 
 # Register your models here.
 admin.site.register(Brand)
 admin.site.register(Item)
+admin.site.register(Shopper)
--- Incopis_Front/wardrobe_manager/models.py
+++ Incopis_Front/wardrobe_manager/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
-# Create your models here.
 class Brand(models.Model):
     name = models.CharField(max_length=200)
 
     def __str__(self):
         return self.name
+
 
 class Item(models.Model):
     description = models.TextField()
@@ -16,3 +16,18 @@ class Item(models.Model):
     def is_cheaper(self, other_item):
         """ Check which item is cheaper"""
         return self.price < other_item.price
+
+    def __str__(self):
+        return "{desc} (From: {brand})".format(
+            desc=self.description,
+            brand=self.brand.name
+        )
+
+class Shopper(models.Model):
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    age = models.IntegerField()
+    items = models.ManyToManyField(Item)
+
+    def __str__(self):
+        return self.name
