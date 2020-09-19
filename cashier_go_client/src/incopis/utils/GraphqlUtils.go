6f2d5fe6a6cc3ba4b1c2692b9601bcc7c4b35a2a
Jolomb <eyalrs21@gmail.com>
2020-09-19 12:00:41 +0300

    Better error handling go

--- cashier_go_client/src/incopis/utils/GraphqlUtils.go
+++ cashier_go_client/src/incopis/utils/GraphqlUtils.go
@@ -6,21 +6,72 @@ import (
 	"github.com/shurcooL/graphql"
 )
 
+type Brand struct {
+	Name graphql.String
+	ID   graphql.ID
+}
 
-func Query() {
-	client := graphql.NewClient("http://127.0.0.1:8000/graphql/", nil)
-	var query struct {
-		AllBrands []struct {
-			Name graphql.String
-			Id graphql.ID
-		}
+type ClientNoInitializedError struct {
+}
+
+func (e *ClientNoInitializedError) Error() string {
+	return "Client not initialized yet!"
+}	
+
+type Item struct {
+	Description graphql.String
+	ID graphql.ID
+	price graphql.Int
+}
+
+type IncopisClient interface {
+	GetAllBrands() ([]Brand, error)
+	InitClient() error
+	GetItemsByBrand(brandId graphql.ID) ([]Item, error)
+}
+
+type DebugClient struct {
+	client_backend *graphql.Client
+}
+
+func (client *DebugClient) InitClient() error {
+	client.client_backend = graphql.NewClient("http://127.0.0.1:8000/graphql/", nil)
+	return nil
+}
+
+func (client *DebugClient) GetItemsByBrand(brandId graphql.ID) ([]Item, error) {
+	if client.client_backend == nil {
+		return nil, &ClientNoInitializedError{}
 	}
-	err := client.Query(context.Background(), &query, nil)
+	return nil, nil
+}
+
+func (client *DebugClient) GetAllBrands() ([]Brand, error) {
+	if client.client_backend == nil {
+		return nil, &ClientNoInitializedError{}
+	}
+	var query struct {
+		AllBrands []Brand
+	}
+
+	err := client.client_backend.Query(context.Background(), &query, nil)
 	if err != nil {
 		fmt.Println("Failed to fetch query: ", err)
+		return nil, err
 	} else {
-		for _, brand := range query.AllBrands {
-			fmt.Println("Brand name: ", brand.Name)
-		}
+		return query.AllBrands, nil
+	}
+}
+
+func Query() {
+	var client DebugClient
+	client.InitClient()
+	brands, err := client.GetAllBrands()
+	if err != nil {
+		fmt.Println("Failed to fetch all brands: ", err)
+	}
+
+	for _, brand := range brands {
+		fmt.Println("Brand name: ", brand.Name)
 	}
 }
